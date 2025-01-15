6a897897f8a56dd60077085e6b74e3e22dc2767f
Gf3p <gf3p00@gmail.com>
2025-01-15 10:23:47 +0100

    Add files via upload

---
+++ CourseAuxPt/src/Objet.java
@@ -0,0 +1,9 @@
+// Class of an object.
+
+class Objet{
+    String nom;         // Name of the object
+    String icon;          // Emoji that represents
+    Effet effet;        // Effect that does
+    int effet_value;    // Effect value (strength)
+    boolean actif;      // Is object active
+}
