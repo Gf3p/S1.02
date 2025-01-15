7652d8cb6a8aa495572c98ce780b2587147906df
Gf3p <gf3p00@gmail.com>
2025-01-15 10:23:05 +0100

    Delete CourseAuxPt directory

--- CourseAuxPt/src/Effet.java
+++
@@ -1,7 +0,0 @@
-// Effects are made by objects that you can find in the game.
-
-enum Effet{
-    VOLER,              // Takes N-amount of points from choosen player.
-    REPONDRE,           // Answers the question and gives you 500 points.
-    INDICE              // Hint. Opens the N first characters in the answer.
-    }
