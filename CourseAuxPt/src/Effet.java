176e6a164fcce8612b30fc907fa7a7821aa66da2
Gf3p <gf3p00@gmail.com>
2025-01-15 16:31:48 +0100

    Add files via upload

---
+++ CourseAuxPt/src/Effet.java
@@ -0,0 +1,7 @@
+// Effects are made by objects that you can find in the game.
+
+enum Effet{
+    NULL,
+    VOLER,              // Takes N-amount of points from choosen player.     
+    TELEPORTER
+}
