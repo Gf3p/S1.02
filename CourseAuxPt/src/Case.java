731e35393f5bcd8ddc8828809752bc43df024967
Gf3p <gf3p00@gmail.com>
2025-01-17 11:29:33 +0100

    Add files via upload

---
+++ CourseAuxPt/src/Case.java
@@ -0,0 +1,5 @@
+class Case{
+    boolean[] directions;          // Possible directions you can go from the cell
+    String contenu;                // Content of the cell
+    boolean accessible;            // Is cell is open to go
+}
---
+++ CourseAuxPt/src/Effet.java
@@ -0,0 +1,8 @@
+// Effects are made by objects that you can find in the game.
+
+enum Effet{
+    NULL,
+    VOLER,              // Takes N-amount of points from choosen player.     
+    TELEPORTERJ,
+    TELEPORTERS
+}
