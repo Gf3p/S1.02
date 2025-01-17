5e30b77b662024eb24f03b22835931849aacc9f2
Gf3p <gf3p00@gmail.com>
2025-01-17 11:27:23 +0100

    Delete CourseAuxPt directory

--- CourseAuxPt/src/Case.java
+++
@@ -1,5 +0,0 @@
-class Case{
-    boolean[] directions;          // Possible directions you can go from the cell
-    String contenu;                // Content of the cell
-    boolean accessible;            // Is cell is open to go
-}
