de5f59664051fca4acd81ae8868540bc57e6baea
Gf3p <gf3p00@gmail.com>
2025-01-21 22:09:23 +0100

    Delete CourseAuxPt/Effet.java

--- CourseAuxPt/Effet.java
+++
@@ -1,8 +0,0 @@
-// Effects are made by objects that you can find in the game.
-
-enum Effet{
-    NULL,
-    VOLER,              // Takes N-amount of points from choosen player.     
-    TELEPORTERJ,
-    TELEPORTERS
-}
